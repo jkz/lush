f5334ba955882626c84b899648811447fcf6bfcd
Hraban Luyat <hraban@0brg.net>
2013-04-04 18:22:09 +0300

    Ringbuffer as io.Writer to remember last n bytes
    Useful for terminal emulator like functionality where you remember the
    last n lines (technically bytes but this suffices for now I think)

---
+++ liblush/ringbuf.go
@@ -0,0 +1,103 @@
+// Copyright © 2013 Hraban Luyat <hraban@0brg.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package liblush
+
+import ()
+
+// Circular fifo buffer.
+// if somebody can point me to an existing implementation I will
+// remove this one. i couldnt find one.
+type ringbuf struct {
+	buf []byte
+	// Oldest byte in the buffer (write starts here)
+	head int
+	// processed bytes
+	seen int
+}
+
+func imin(i int, rest ...int) int {
+	if len(rest) == 0 {
+		return i
+	}
+	j := rest[0]
+	if j < i {
+		i = j
+	}
+	return imin(i, rest[1:]...)
+}
+
+func newRingbuf(size int) *ringbuf {
+	return &ringbuf{
+		buf: make([]byte, size),
+	}
+}
+
+func (r *ringbuf) Size() int {
+	return len(r.buf)
+}
+
+// Fill p with most recently written bytes. Returns number of bytes written to
+// p.
+func (r *ringbuf) Last(p []byte) (n int) {
+	// dont ask for more than what i got
+	want := imin(len(p), len(r.buf), r.seen)
+	// actual copying
+	// easiest scenario 1 step
+	if want <= r.head {
+		n = copy(p, r.buf[r.head-want:r.head])
+		return
+	}
+	// otherwise 2 steps (here is first part)
+	n = copy(p, r.buf[len(r.buf)-(want-r.head):])
+	// append the rest
+	n += copy(p[n:], r.buf[:r.head])
+	if n != want {
+		panic("unexpected copy length")
+	}
+	return
+}
+
+func (r *ringbuf) Write(p []byte) (n int, err error) {
+	inputlen := len(p)
+	defer func() {
+		if err == nil {
+			r.seen += inputlen
+		}
+	}()
+	overflow := len(p) - len(r.buf)
+	if overflow > 0 {
+		// only care about last bytes anyway
+		p = p[overflow:]
+	}
+	// size of first free data block
+	part1 := len(r.buf) - r.head
+	// first option: theres enough space left to copy in 1 block
+	if len(p) <= part1 {
+		n = copy(r.buf[r.head:], p)
+		r.head += n
+		return
+	}
+	// last resort: split my buf in 2 pieces
+	n = copy(r.buf[r.head:], p[:part1])
+	r.head = copy(r.buf[:r.head], p[part1:])
+	n += r.head
+	return
+}
---
+++ liblush/ringbuf_test.go
@@ -0,0 +1,67 @@
+// Copyright © 2013 Hraban Luyat <hraban@0brg.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package liblush
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestRingbuf(t *testing.T) {
+	r := newRingbuf(5)
+	buf := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	n, err := r.Write(buf)
+	if err != nil {
+		t.Fatalf("Write error: %v", err)
+	}
+	if n != 5 {
+		t.Error("Unexpected write byte-count: ", n)
+	}
+	buf2 := make([]byte, 3)
+	n = r.Last(buf2)
+	buf2 = buf2[:n]
+	if n != 3 {
+		t.Errorf("Filled read buffer with %d bytes expected 3", n)
+	}
+	if !bytes.Equal(buf2, []byte{7, 8, 9}) {
+		t.Error("Unexpected last bytes: ", buf2)
+	}
+	buf3 := []byte{10, 11, 12}
+	n, err = r.Write(buf3)
+	if err != nil {
+		t.Fatalf("Write error: %v", err)
+	}
+	if n != 3 {
+		t.Error("Unexpected write byte-count: ", n)
+	}
+	buf4 := make([]byte, 20)
+	n = r.Last(buf4)
+	buf4 = buf4[:n]
+	if n != 5 {
+		t.Errorf("Filled read buffer with %d bytes expected 5", n)
+	}
+	if !bytes.Equal(buf4, []byte{8, 9, 10, 11, 12}) {
+		t.Error("Unexpected last bytes: ", buf4)
+	}
+	if r.seen != 13 {
+		t.Error("Saw 13 bytes, recorded ", r.seen)
+	}
+}
