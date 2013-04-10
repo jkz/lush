86a014cb63064894d8df90a16ec0a4f5004decc1
Hraban Luyat <hraban@0brg.net>
2013-04-10 19:11:15 +0200

    Insert (useless) box between every new connection
    can later be used to monitor stream data

--- static/js/root.js
+++ static/js/root.js
@@ -32,6 +32,56 @@ var restoreposition = function(id) {
     }
 };
 
+var streampeekerId = 0;
+
+// Stream peeker is like a small dumb terminal window showing a stream's most
+// recent output
+var addstreampeeker = function() {
+    var id = 'streampeeker' + (streampeekerId++);
+    var $sp = $('<div class=streampeeker id=' + id + '>')
+        .resizable()
+        .appendTo('body');
+    // open / collapse button
+    var $ocbutton = $('<button>');
+    // functions that open / collapse the streampeeker
+    var openf, collapsef;
+    openf = function() {
+        $sp.removeClass('collapsed');
+        $sp.addClass('open');
+        $ocbutton.text('▬');
+        $ocbutton.unbind('click', openf);
+        $ocbutton.bind('click', collapsef);
+        $sp.resizable({
+            resize: function(e, ui) {
+                jsPlumb.repaint(ui.helper);
+            }})
+        jsPlumb.repaint($sp);
+    };
+    collapsef = function() {
+        $sp.removeClass('open');
+        $sp.addClass('collapsed');
+        $ocbutton.text('◳');
+        $ocbutton.unbind('click', collapsef);
+        $ocbutton.bind('click', openf);
+        $sp.resizable('destroy');
+        jsPlumb.repaint($sp);
+    };
+    collapsef();
+    jsPlumb.draggable($sp);
+    $sp.append($ocbutton);
+    var left = jsPlumb.addEndpoint(id, {
+        anchor: 'TopCenter',
+        isTarget: true,
+        endpoint: 'Rectangle',
+    });
+    jsPlumb.addEndpoint(id, {
+        anchor: 'BottomCenter',
+        isSource: true,
+        endpoint: 'Rectangle',
+    });
+    return $sp;
+};
+
 var stream2anchor = function(stream) {
     return {stderr: "RightMiddle", stdout: "BottomCenter"}[stream]
 };
@@ -40,23 +90,37 @@ var anchor2stream = function(anchor) {
     return {RightMiddle: "stderr", BottomCenter: "stdout"}[anchor];
 };
 
-var connectVisually = function(srcId, trgtId, stream) {
+var connectVisually = function($src, $trgt, stream, withstreampeeker) {
+    var anchor = stream2anchor(stream);
+    if (withstreampeeker) {
+        var $sp = addstreampeeker();
+        jsPlumb.connect({
+            source: $src,
+            target: $sp,
+            anchors: [anchor, "TopCenter"],
+        });
+        $src = $sp;
+        anchor = "BottomCenter";
+    }
     jsPlumb.connect({
-        source: srcId,
-        target: trgtId,
-        anchors: [stream2anchor(stream), "TopCenter"],
+        source: $src,
+        target: $trgt,
+        anchors: [anchor, "TopCenter"],
     });
 };
 
-var connect = function(srcId, trgtId, stream) {
-    srcNId = +srcId.substring(3);
-    trgtNId = +trgtId.substring(3);
-    $.post('/' + srcNId + '/connect?noredirect', {
+var connect = function(srcSysId, trgtSysId, stream) {
+    $.post('/' + srcSysId + '/connect?noredirect', {
         stream: stream,
-        to: trgtNId,
+        to: trgtSysId,
     }).done(function() {
-        connectVisually(srcId, trgtId, stream);
+        connectVisually('cmd' + srcSysId, 'cmd' + trgtSysId, stream, true);
     });
+};
+
+// analogous to CL's function by the same name
+var constantly = function(val) {
+    return function() { return val; }
 };
 
 $(document).ready(function() {
@@ -79,32 +143,44 @@ $(document).ready(function() {
         jsPlumb.addEndpoint(cmd.htmlid, {
             anchor: 'TopCenter',
             isTarget: true,
+            parameters: {
+                sysid: constantly(cmd.nid),
+            },
         });
         jsPlumb.addEndpoint(cmd.htmlid, {
             anchor: 'BottomCenter',
             isSource: true,
+            parameters: {
+                stream: constantly("stdout"),
+                sysid: constantly(cmd.nid),
+            },
         });
         jsPlumb.addEndpoint(cmd.htmlid, {
             anchor: 'RightMiddle',
             isSource: true,
+            parameters: {
+                stream: constantly("stderr"),
+                sysid: constantly(cmd.nid),
+            },
         });
     });
     // Second iteration to ensure that connections are only made after all
     // nodes have configured endpoints
     $.map(cmds, function(cmd, i) {
         if (cmd.hasOwnProperty('stdoutto')) {
-            connectVisually(cmd.htmlid, 'cmd' + cmd.stdoutto, 'stdout');
+            connectVisually(cmd.htmlid, 'cmd' + cmd.stdoutto, 'stdout', false);
         }
         if (cmd.hasOwnProperty('stderrto')) {
-            connectVisually(cmd.htmlid, 'cmd' + cmd.stderrto, 'stderr');
+            connectVisually(cmd.htmlid, 'cmd' + cmd.stderrto, 'stderr', false);
         }
     });
     jsPlumb.importDefaults({ConnectionsDetachable: false});
     jsPlumb.bind("beforeDrop", function(info) {
+        // Connected to another command
         connect(
-            info.connection.sourceId,
-            info.connection.targetId,
-            anchor2stream(info.connection.endpoints[0].anchor.type));
+            info.connection.endpoints[0].getParameter("sysid")(),
+            info.dropEndpoint.getParameter("sysid")(),
+            info.connection.getParameter("stream")());
         return false;
     });
     // ajaxify start command button
