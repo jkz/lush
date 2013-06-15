ef16b0e04747d77ae1db29698f2972a39029fb1a
Hraban Luyat <hraban@0brg.net>
2013-06-15 16:32:29 +0200

    Unify signature of websocket event handlers

--- websocket.go
+++ websocket.go
@@ -29,7 +29,13 @@ import (
 )
 
 // subscribe all websocket clients to stream data
-func wseventSubscribe(s *server, idstr, streamname string) error {
+func wseventSubscribe(s *server, options string) error {
+	args := strings.Split(options, ";")
+	if len(args) != 2 {
+		return errors.New("subscribe requires 2 args")
+	}
+	idstr := args[0]
+	streamname := args[1]
 	id, _ := liblush.ParseCmdId(idstr)
 	c := s.session.GetCommand(id)
 	if c == nil {
@@ -90,7 +96,7 @@ func wseventSetpath(s *server, pathJSON string) error {
 	return err
 }
 
-func wseventGetpath(s *server) error {
+func wseventGetpath(s *server, _ string) error {
 	w := newPrefixedWriter(&s.ctrlclients, []byte("path;"))
 	return json.NewEncoder(w).Encode(getPath())
 }
@@ -103,11 +109,7 @@ func parseAndHandleWsEvent(s *server, msg []byte) error {
 	switch argv[0] {
 	case "subscribe":
 		// eg subscribe;3;stdout
-		args := strings.Split(argv[1], ";")
-		if len(args) != 2 {
-			return errors.New("subscribe requires 2 args")
-		}
-		return wseventSubscribe(s, args[0], args[1])
+		return wseventSubscribe(s, argv[1])
 	case "new":
 		// eg new;{"cmd":"echo","args":["arg1","arg2"],...}
 		return wseventNew(s, argv[1])
@@ -116,7 +118,7 @@ func parseAndHandleWsEvent(s *server, msg []byte) error {
 		return wseventSetpath(s, argv[1])
 	case "getpath":
 		// eg getpath;
-		return wseventGetpath(s)
+		return wseventGetpath(s, argv[1])
 	}
 	return errors.New("unknown command")
 }
