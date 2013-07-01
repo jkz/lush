e306acf0cce94cb9d1e389e3593e00c99b3acc76
Hraban Luyat <hraban@0brg.net>
2013-07-01 12:06:04 +0200

    work on new parser API through unit tests
    not done yet but i think this is a step in the right direction

--- static/js/lush/tests.js
+++ static/js/lush/tests.js
@@ -38,22 +38,86 @@ define(["lush/parser", "lush/utils"], function (Parser) {
         deepEqual("".splitn("", 1), []);
     });
     
+    // these tests are part of a wip to define the api of the parser (they are
+    // supposed to fail)
     test("parser", function() {
-        var glob = function () { return []; };
-        var parser = new Parser(glob);
-        var res, argv;
-        // simple
-        res = parser.parse("foo bar baz");
-        argv = res.map(attrgetter("text"));
-        deepEqual(argv, ['foo', 'bar', 'baz'], 'simple parsing');
-        // single quotes
-        res = parser.parse("foo 'bar baz'");
-        argv = res.map(attrgetter("text"));
-        deepEqual(argv, ['foo', 'bar baz'], 'single quotes');
-        // double quotes
-        res = parser.parse('foo "bar baz"');
-        argv = res.map(attrgetter("text"));
-        deepEqual(argv, ['foo', 'bar baz'], 'double quotes');
+        // simple parser callback: store all words in argv
+        var argv;
+        var callback = {
+            init: function () { argv = []; },
+            rawStr: function (str) { argv.push(str); },
+            globStr: function (str) { argv.push(str); },
+        };
+        var parser = new Parser(callback);
+        var t = function (raw, out, name) {
+            parser.parse(raw);
+            deepEqual(argv, out, name);
+        };
+        t("foo bar baz", ['foo', 'bar', 'baz'], 'simple parsing');
+        t("foo 'bar baz'", ['foo', 'bar baz'], 'single quotes');
+        t('foo "bar baz"', ['foo', 'bar baz'], 'double quotes');
+        t('foo "bar"baz', ['foo', 'barbaz'], 'concatenated words');
+        t('foo "bar"\'\'b""""az', ['foo', 'barbaz'], 'concatenated quotes');
+        t('"\'"', ["'"], 'quoted single quote');
+        t("'\"'", ['"'], 'quoted double quote');
+    });
 
+    // these tests are part of a wip to define the api of the prompt (they are
+    // supposed to fail)
+    test("command-line interaction", function() {
+        // live command object connected to current prompt
+        var cmd;
+        var initNum = 0;
+        // called for every fresh prompt line
+        var initFunc = function (callback) {
+            initNum += 1;
+            cmd = {};
+            callback(cmd);
+        };
+        var cli = new Prompt(initFunc);
+        // array of suggestions (during tab completion)
+        var suggestions;
+        // called by prompt to indicate it wants to suggest something to user
+        cli.onsuggest = function (x) {
+            suggestions = x;
+        };
+
+        // tab completion
+        cli.setprompt('ls foo');
+        cli.tab();
+        deepEqual(suggestions, ['foo1.txt', 'foo2.txt'], 'tab completion');
+        // init function
+        equal(initNum, 1, 'prompt init callback called once');
+
+        // globbing
+        cli.setprompt('foo*');
+        equal(initNum, 1, 'prompt init callback not called again');
+        cli.enter();
+        deepEqual(cli.argv, ['foo1.txt', 'foo2.txt'], 'suffix *');
+
+        cli.setprompt('foo*txt');
+        equal(initNum, 2, 'prompt init callback called >1');
+        cli.enter();
+        deepEqual(cli.argv, ['foo1.txt', 'foo2.txt'], '* in the middle');
+
+        cli.setprompt('*.txt');
+        equal(initNum, 3, 'prompt init callback called >1');
+        cli.enter();
+        deepEqual(cli.argv, ['foo1.txt', 'foo2.txt'], 'prefix *');
+
+        cli.setprompt('f*o*xt');
+        equal(initNum, 4, 'prompt init callback called >1');
+        cli.enter();
+        deepEqual(cli.argv, ['foo1.txt', 'foo2.txt'], 'multiple *s');
+
+        cli.setprompt('foo?.txt');
+        equal(initNum, 5, 'prompt init callback called >1');
+        cli.enter();
+        deepEqual(cli.argv, ['foo1.txt', 'foo2.txt'], '? in the middle');
+
+        cli.setprompt('"foo*"');
+        equal(initNum, 6, 'prompt init callback called >1');
+        cli.enter();
+        deepEqual(cli.argv, ['foo*'], 'wildcard in quotes');
     });
 });
