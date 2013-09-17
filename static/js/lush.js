1750f322388c2130507ea4c59e16fe13fde3d2a5
Hraban Luyat <hraban@0brg.net>
2013-09-17 23:11:04 +0200

    upgrade to jquery terminal 0.7.6

--- static/js/lush.js
+++ static/js/lush.js
@@ -45,7 +45,7 @@ requirejs.config({
       "lush": "../lush",
       "jquery": "jquery-2.0.3",
       "jquery.ui": "jquery-ui-1.10.2.min",
-      "jquery.terminal": "jquery.terminal-0.6.3.min",
+      "jquery.terminal": "jquery.terminal-0.7.6.min",
       "jsPlumb": "jquery.jsPlumb-1.3.16-all-min",
     },
     "shim": {
