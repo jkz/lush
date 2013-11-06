820cda2fa5ca48d4ec7cf3514478f1932e88d446
Hraban Luyat <hraban@0brg.net>
2013-11-06 17:19:32 +0100

    create runnable test shell script

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+go test ./... || exit 1
+
+phantompath="$(which phantomjs)"
+if [[ -z "$phantompath" ]]
+then
+	echo Need phantomjs in PATH for unit testing qunit >&2
+	exit 1
+fi
+
+# start a lush server
+go build || exit 1
+./lush -l 127.0.0.1:4737 &
+lushpid=$!
+sleep 3
+
+phantomjs phantom-qunit-runner.js http://127.0.0.1:4737/test.html
+phantomexit=$?
+
+kill $lushpid
+exit $phantomexit
