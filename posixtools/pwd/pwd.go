9acfa93f926c435eac4c0b1250787ca09a54909f
Hraban Luyat <hraban@0brg.net>
2014-01-03 18:06:48 +0100

    simple pwd and echo tools (for windows)
    fixes hraban/lush#28
    fixes hraban/lush#27

---
+++ posixtools/pwd/pwd.go
@@ -0,0 +1,46 @@
+// Copyright © 2014 Hraban Luyat <hraban@0brg.net>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+package main
+
+import (
+    "os"
+    "fmt"
+)
+
+func printerr(err error) {
+    fmt.Fprintf(os.Stderr, "%v\n", err)
+    os.Exit(1)
+}
+
+func main() {
+    //_, err := fmt.Println(os.Args[1:]...)
+    // cannot use os.Args (type []string) as type []interface {} in function
+    // argument
+    // OHMYGOD GO WHAT IS WRONG WITH YOU
+    wd, err := os.Getwd()
+    if err != nil {
+	printerr(err)
+    }
+    _, err = fmt.Println(wd)
+    if err != nil {
+	printerr(err)
+    }
+}
